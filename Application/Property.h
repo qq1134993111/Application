f8eae90cd8a717569b49601ab124850e20edf1d7
qq1134993111 <1134993111@qq.com>
2018-06-13 21:30:19 +0800

    修改

--- Application/Property.h
+++ Application/Property.h
@@ -6,6 +6,7 @@
 
 #include<boost/any.hpp>
 #include<boost/optional.hpp>
+#include <unordered_set>
 
 namespace general
 {
@@ -16,28 +17,100 @@ namespace general
 	public:
 
 		template<typename U>
-		PropertyT operator()(const T& key, const U& value)
+		PropertyT& operator()(const T& key, const U& value)
+		{
+			return SetValue(key, value);
+		}
+
+		template<typename U>
+		typename std::enable_if<
+			!(std::is_same<bool, U>::value ||
+				std::is_same<char, U>::value ||
+				std::is_same<unsigned char, U>::value ||
+				std::is_same<int8_t, U>::value ||
+				std::is_same<uint8_t, U>::value ||
+				std::is_same<int16_t, U>::value ||
+				std::is_same<uint16_t, U>::value ||
+				std::is_same<int32_t, U>::value ||
+				std::is_same<uint32_t, U>::value ||
+				std::is_same<int64_t, U>::value ||
+				std::is_same<uint64_t, U>::value ||
+				std::is_same<float, U>::value ||
+				std::is_same<double, U>::value),
+			PropertyT&>::type
+			SetValue(const T& key, const U& value)
 		{
 			prop_any_umap_[key] = value;
 			return *this;
 		}
 
 		template<typename U>
-		PropertyT SetValue(const T& key, const U& value)
+		typename std::enable_if<
+			std::is_same<bool, U>::value ||
+			std::is_same<char, U>::value ||
+			std::is_same<unsigned char, U>::value ||
+			std::is_same<int8_t, U>::value ||
+			std::is_same<uint8_t, U>::value ||
+			std::is_same<int16_t, U>::value ||
+			std::is_same<uint16_t, U>::value ||
+			std::is_same<int32_t, U>::value ||
+			std::is_same<uint32_t, U>::value ||
+			std::is_same<int64_t, U>::value ||
+			std::is_same<uint64_t, U>::value,
+			PropertyT>::type
+			SetValue(const T& key, const U& value)
 		{
-			prop_any_umap_[key] = value;
+			prop_any_umap_[key] = (uint64_t)value;
+			return *this;
+		}
+
+		template<typename U>
+		typename std::enable_if<
+			std::is_same<float, U>::value ||
+			std::is_same<double, U>::value,
+			PropertyT&>::type
+			SetValue(const T& key, const U& value)
+		{
+			prop_any_umap_[key] = (double)value;
 			return *this;
 		}
 
 		template<typename U>
 		U GetValue(const T& key, const U& default_value) const
 		{
+
+			boost::optional<U> value = GetValue<U>(key);
+			return value.value_or(default_value);
+		}
+
+
+		template<typename U>
+		typename std::enable_if<
+			!(std::is_same<bool, U>::value ||
+				std::is_same<char, U>::value ||
+				std::is_same<unsigned char, U>::value ||
+				std::is_same<int8_t, U>::value ||
+				std::is_same<uint8_t, U>::value ||
+				std::is_same<int16_t, U>::value ||
+				std::is_same<uint16_t, U>::value ||
+				std::is_same<int32_t, U>::value ||
+				std::is_same<uint32_t, U>::value ||
+				std::is_same<int64_t, U>::value ||
+				std::is_same<uint64_t, U>::value ||
+				std::is_same<float, U>::value ||
+				std::is_same<double, U>::value),
+			boost::optional<U>>
+			::type
+			GetValue(const T& key) const
+		{
 			try
 			{
 				auto it = prop_any_umap_.find(key);
 				if (it != prop_any_umap_.end())
 				{
+
 					return boost::any_cast<U>(it->second);
+
 				}
 
 			}
@@ -46,18 +119,56 @@ namespace general
 
 			}
 
-			return default_value;
+			return {};
 		}
 
 		template<typename U>
-		boost::optional<U> GetValue(const T& key) const
+		typename std::enable_if<
+			std::is_same<bool, U>::value ||
+			std::is_same<char, U>::value ||
+			std::is_same<unsigned char, U>::value ||
+			std::is_same<int8_t, U>::value ||
+			std::is_same<uint8_t, U>::value ||
+			std::is_same<int16_t, U>::value ||
+			std::is_same<uint16_t, U>::value ||
+			std::is_same<int32_t, U>::value ||
+			std::is_same<uint32_t, U>::value ||
+			std::is_same<int64_t, U>::value ||
+			std::is_same<uint64_t, U>::value,
+			boost::optional<U>>
+			::type
+			GetValue(const T& key) const
 		{
 			try
 			{
 				auto it = prop_any_umap_.find(key);
 				if (it != prop_any_umap_.end())
 				{
-					return boost::any_cast<U>(it->second);
+					return static_cast<U>(boost::any_cast<uint64_t>(it->second));
+				}
+
+			}
+			catch (...)
+			{
+
+			}
+
+			return {};
+		}
+
+		template<typename U>
+		typename std::enable_if<
+			std::is_same<float, U>::value ||
+			std::is_same<float, U>::value,
+			boost::optional<U>>::type
+			GetValue(const T& key) const
+		{
+			try
+			{
+				auto it = prop_any_umap_.find(key);
+				if (it != prop_any_umap_.end())
+				{
+					return boost::any_cast<double>(it->second);
 				}
 
 			}
@@ -86,28 +197,28 @@ namespace general
 		~Property() {}
 
 		template<typename U>
-		Property operator()(uint32_t key, const U& value)
+		Property& operator()(uint32_t key, const U& value)
 		{
 			prop_i_.SetValue(key, value);
 			return *this;
 		}
 
 		template<typename U>
-		Property operator()(const std::string& key, const U& value)
+		Property& operator()(const std::string& key, const U& value)
 		{
 			prop_s_.SetValue(key, value);
 			return *this;
 		}
 
 		template<typename U>
-		Property SetValue(uint32_t key, const U& value)
+		Property& SetValue(uint32_t key, const U& value)
 		{
 			prop_i_.SetValue(key, value);
 			return *this;
 		}
 
 		template<typename U>
-		Property SetValue(const std::string& key, const U& value)
+		Property& SetValue(const std::string& key, const U& value)
 		{
 			prop_s_.SetValue(key, value);
 			return *this;
@@ -118,6 +229,7 @@ namespace general
 		{
 			return prop_i_.GetValue(key, default_value);
 		}
+
 
 		template<typename U>
 		U GetValue(const std::string& key, const U& default_value) const
@@ -147,140 +259,6 @@ namespace general
 		PropertyT<std::string> prop_s_;
 	};
 
-
-	//class Property
-	//{
-	//public:
-	//
-	//	Property operator()(const std::string& key, bool value)
-	//	{
-	//		prop_int_map_[key] = (uint64_t)value;
-	//		return *this;
-	//	}
-	//	Property operator()(const std::string& key, int32_t value)
-	//	{
-	//		prop_int_map_[key] = (uint64_t)value;
-	//		return *this;
-	//	}
-	//	Property operator()(const std::string& key, uint32_t value)
-	//	{
-	//		prop_int_map_[key] = (uint64_t)value;
-	//		return *this;
-	//	}
-	//	Property operator()(const std::string& key, int64_t value)
-	//	{
-	//		prop_int_map_[key] = (uint64_t)value;
-	//		return *this;
-	//	}
-	//	Property operator()(const std::string& key, uint64_t value)
-	//	{
-	//		prop_int_map_[key] = value;
-	//		return *this;
-	//	}
-	//
-	//	Property operator()(const std::string& key, const std::string& value)
-	//	{
-	//		prop_string_map_[key] = value;
-	//		return *this;
-	//	}
-	//	Property operator()(const std::string& key, const char* value)
-	//	{
-	//		prop_string_map_[key] = value;
-	//		return *this;
-	//	}
-	//	Property operator()(const std::string& key, std::nullptr_t pointer)
-	//	{
-	//		prop_pointer_map_[key] = pointer;
-	//		return *this;
-	//	}
-	//
-	//	template<typename pointer_type>
-	//	Property operator()(const std::string& key, pointer_type* const point)
-	//	{
-	//		prop_pointer_map_[key] = (void)point;
-	//		return *this;
-	//	}
-	//
-	//	bool GetValue(const std::string& key, bool default_value) const
-	//	{
-	//		auto iter = prop_int_map_.find(key);
-	//		if (iter != prop_int_map_.end())
-	//		{
-	//			return (bool)(iter->second);
-	//		}
-	//		return default_value;
-	//	}
-	//
-	//	int32_t GetValue(const std::string& key, int32_t default_value) const
-	//	{
-	//		auto iter = prop_int_map_.find(key);
-	//		if (iter != prop_int_map_.end())
-	//		{
-	//			return (int32_t)(iter->second);
-	//		}
-	//		return default_value;
-	//	}
-	//	uint32_t GetValue(const std::string& key, uint32_t default_value) const
-	//	{
-	//		auto iter = prop_int_map_.find(key);
-	//		if (iter != prop_int_map_.end())
-	//		{
-	//			return (uint32_t)(iter->second);
-	//		}
-	//		return default_value;
-	//	}
-	//
-	//	int64_t GetValue(const std::string& key, int64_t default_value) const
-	//	{
-	//		auto iter = prop_int_map_.find(key);
-	//		if (iter != prop_int_map_.end())
-	//		{
-	//			return (int64_t)(iter->second);
-	//		}
-	//		return default_value;
-	//	}
-	//	uint64_t GetValue(const std::string& key, uint64_t default_value) const
-	//	{
-	//		auto iter = prop_int_map_.find(key);
-	//		if (iter != prop_int_map_.end())
-	//		{
-	//			return (uint64_t)(iter->second);
-	//		}
-	//		return default_value;
-	//	}
-	//	std::string GetValue(const std::string& key, const std::string& default_value) const
-	//	{
-	//		auto iter = prop_string_map_.find(key);
-	//		if (iter != prop_string_map_.end())
-	//		{
-	//			return  iter->second;
-	//		}
-	//		return default_value;
-	//	}
-	//
-	//	template<typename pointer_type>
-	//	pointer_type GetPointer(const std::string& key) const
-	//	{
-	//		auto iter = prop_pointer_map_ find(key);
-	//		if (iter != prop_pointer_map_.end())
-	//		{
-	//			return reinterpret_cast<pointer_type(iter->second);
-	//		}
-	//		return nullptr;
-	//	}
-	//
-	//	void Clear()
-	//	{
-	//		prop_int_map_.clear();
-	//		prop_string_map_.clear();
-	//		prop_pointer_map_.clear();
-	//	}
-	//
-	//private:
-	//	std::unordered_map<std::string, uint64_t> prop_int_map_;
-	//	std::unordered_map<std::string, std::string> prop_string_map_;
-	//	std::unordered_map<std::string, void*> prop_pointer_map_;
-	//};
 
 
 }
